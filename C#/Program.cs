3e408d34464796bbfa554a4fb9bf3b4c1bc2fbfc
mariuszkuswik <78mariuszsz@gmail.com>
2022-08-22 11:35:15 +0200

    C#_ogarniete_pliki

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,20 +1,12 @@
-﻿
-double posiadaneJanek=0, dlugJanek=80, zarobekJanek=50;
-double posiadaneKarol=0, dlugKarol=80, zarobekKarol=40;
+using System;
 
-int liczbaDni=0;
-
-do
+public class HelloWorld
 {
-    Console.WriteLine("Janek posiada {0}", posiadaneJanek);
-    Console.WriteLine("Karol posiada {0}", posiadaneKarol);
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Test");
+        
 
-    posiadaneJanek+=zarobekJanek*0.2;
-    posiadaneKarol+=zarobekKarol*0.2;
-
-    liczbaDni++; 
-    .
+    }
 }
-while(posiadaneJanek<=dlugJanek || posiadaneKarol<=dlugKarol);
-
-Console.WriteLine(liczbaDni);
